10e7d32e72a36993c4860bdc284e1ecf9de5db74
namistarr <namistarr@icloud.com>
2019-09-05 12:50:04 -0700

    made dynamic port and added env

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // code away!
+require('dotenv').config()
 const server = require('./server.js');
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 server.listen(port, () => {
     console.log(`Server listening on port ${port}!`)
 })
--- knexfile.js
+++ knexfile.js
@@ -16,6 +16,25 @@ module.exports = {
     },
     seeds: {
       directory: './data/seeds',
-    },
+    }
   },
+  production: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/blog.db3',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'knex_migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    }
+  }
 };
